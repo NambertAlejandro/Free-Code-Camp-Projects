6a2481422af597ffcf21c69c9f264d84e1043dc7
NambertAlejandro <nambertprofessional.org@gmail.com>
2026-08-07 22:58:24 -0300

    Initial commit: freeCodeCamp projects (trivia bot, phrase creator)

---
+++ project01.bot,trivia.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Byte";
+const botLocation = "the Cloud";
+const favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `${favoriteLanguage} was created in just 10 days by Brendan Eich in 1995.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} was originally called Mocha, then LiveScript, before being renamed to ${favoriteLanguage}.`;
+console.log(codingFact);
+
+codingFact = `Despite its name, ${favoriteLanguage} has no direct relation to the Java programming language.`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
